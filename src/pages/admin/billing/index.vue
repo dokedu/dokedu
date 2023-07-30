6529a25ce387181466284ff247b0b06f072c8fc3
Tom Härter <tom@dokedu.org>
2023-07-30 02:18:04 +0200

    Update billing page layout and pricing information
    - Added a new div element with a message indicating that the page is still a work in progress

--- src/pages/admin/billing/index.vue
+++ src/pages/admin/billing/index.vue
@@ -1,16 +1,21 @@
 <template>
-  <page-wrapper>
+  <page-wrapper class="relative">
     <page-header class="select-none justify-between">
       <div class="flex items-center gap-4">
         <div class="font-medium text-stone-950">{{ $t("billing") }}</div>
       </div>
     </page-header>
+    <div
+      class="absolute left-0 top-0 z-10 flex h-full w-full select-none items-center justify-center bg-stone-500/10 font-semibold text-stone-500 backdrop-blur-sm"
+    >
+      <div>This is still a work in progress. Please contact us if you want to change your plan.</div>
+    </div>
     <div class="flex select-none gap-4 px-8 py-4 text-sm text-subtle">
       <div class="w-full max-w-xl">
         <div class="flex w-full items-center justify-between gap-8 rounded-lg bg-stone-100 p-4">
           <div class="flex flex-col gap-1">
             <div class="font-medium text-default">Standard</div>
-            <div class="text-lg text-default">10 €</div>
+            <div class="text-lg text-default">5 €</div>
             <div class="text-xs">{{ $t("per_member_monthly") }}</div>
           </div>
           <div>
