997f8f9f3a1d09139e0b9969a397d9fc8442236e
Tom Härter <tom@dokedu.org>
2023-08-11 15:33:14 +0200

    Fix signOut function

--- src/composables/auth.ts
+++ src/composables/auth.ts
@@ -1,31 +1,35 @@
 import { graphql } from "@/gql";
 import { urqlClient } from "@/main";
+import router from "@/router";
 
 
 function signOutMutation() {
-    return urqlClient.mutation(
-        graphql(`
+  return urqlClient.mutation(
+    graphql(`
     mutation signOut {
       signOut
     }
   `), {}
-    )
+  )
 }
 
 
 
 async function signOut() {
+  try {
     const res = await signOutMutation();
     if (res.error) return console.error(res.error);
+  } catch (err) {
+    console.error(err);
+  }
 
-    // Clear the token and enabled_apps from local storage
-    localStorage.removeItem("token");
-    localStorage.removeItem("enabled_apps");
+  localStorage.clear()
+  await router.push({ name: "/login" });
+  // ensure urql cache is cleared
+  location.reload();
 
-    // TODO: hack to ensure urql cache is cleared
-    location.reload();
 }
 
 export function useAuth() {
-    return { signOut }
+  return { signOut }
 }
