e518bf46299dcf683845d5ae3f2bc020d64ce06a
Aaron Mahlke <aaron.mahlke@gmail.com>
2024-01-09 22:22:20 +0100

    Update error handling for password-reset

--- src/pages/reset-password.vue
+++ src/pages/reset-password.vue
@@ -4,44 +4,46 @@
     :subtitle="$t('reset_password_description')"
   >
     <template #banner>
-      <!-- <d-banner v-if="successBanner" type="success" :title="t('password_set_successfully')"></d-banner> -->
-      <!-- <d-banner v-if="errorBanner" type="error" :title="errorTitle"></d-banner> -->
+      <d-banner v-if="successBanner" type="success" :title="t('password_set_successfully')"></d-banner>
+      <d-banner v-if="errorBanner" type="error" :title="errorTitle"></d-banner>
     </template>
     <template #form>
       <form @submit.prevent="onSubmit" class="flex flex-col gap-5">
-        <d-input
-          :label="$t('email')"
-          type="email"
-          name="email"
-          :disabled="true"
-          :modelValue="computedInviteDetails?.email"
-          :message="$t('your_login_mail')"
-        ></d-input>
-        <d-input
-          size="sm"
-          :label="$t('password')"
-          v-model="password"
-          type="password"
-          name="password"
-          id="password"
-          required
-          :min="8"
-          :placeholder="$t('your_new_password')"
-        ></d-input>
-        <d-input
-          size="sm"
-          :label="$t('confirm_password')"
-          v-model="passwordConfirm"
-          type="password"
-          name="confirm-password"
-          id="confirm-password"
-          required
-          :min="8"
-          :placeholder="$t('confirm_your_new_password')"
-        ></d-input>
-        <d-button type="primary" submit>
-          {{ $t("reset_password") }}
-        </d-button>
+        <div v-if="!successBanner" class="flex flex-col gap-5">
+          <d-input
+            :label="$t('email')"
+            type="email"
+            name="email"
+            :disabled="true"
+            :modelValue="computedInviteDetails?.email"
+            :message="$t('your_login_mail')"
+          ></d-input>
+          <d-input
+            size="sm"
+            :label="$t('password')"
+            v-model="password"
+            type="password"
+            name="password"
+            id="password"
+            required
+            :min="8"
+            :placeholder="$t('your_new_password')"
+          ></d-input>
+          <d-input
+            size="sm"
+            :label="$t('confirm_password')"
+            v-model="passwordConfirm"
+            type="password"
+            name="confirm-password"
+            id="confirm-password"
+            required
+            :min="8"
+            :placeholder="$t('confirm_your_new_password')"
+          ></d-input>
+          <d-button type="primary" submit>
+            {{ $t("reset_password") }}
+          </d-button>
+        </div>
         <router-link
           class="mx-auto mt-2 block w-fit rounded-md text-center text-xs font-medium leading-none text-muted hover:text-default focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-950"
           to="/login"
@@ -89,22 +91,28 @@ const computedInviteDetails = computed(() => inviteDetails?.value?.inviteDetails
 
 const password = ref("")
 const passwordConfirm = ref("")
+const successBanner = ref(false)
+const errorBanner = ref(false)
+const errorTitle = ref("")
 
 async function onSubmit() {
   const token = route.hash.slice(1).split("&")[0].split("=")[1]
 
   if (password.value !== passwordConfirm.value) {
-    alert(t("passwords_dont_match"))
+    errorTitle.value = t("passwords_dont_match")
+    errorBanner.value = true
     return
   }
 
   if (!token) {
-    alert(t("invalid_token"))
+    errorTitle.value = t("invalid_token")
+    errorBanner.value = true
     return
   }
 
   if (password.value.length < 8) {
-    alert(t("password_too_short"))
+    errorTitle.value = t("password_too_short")
+    errorBanner.value = true
     return
   }
 
@@ -116,8 +124,11 @@ async function onSubmit() {
   })
 
   if (data?.resetPassword?.success) {
-    alert(t("password_reset_successfully"))
-    await router.push("/login")
+    errorBanner.value = false
+    successBanner.value = true
+    setTimeout(() => {
+      router.push("/login")
+    }, 3000)
   } else {
     alert(t("something_went_wrong"))
   }
