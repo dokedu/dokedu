fd11bd2fd68dbf9ac14e544f010a1a2f2142564d
Aaron Mahlke <aaron.mahlke@gmail.com>
2024-01-18 15:52:27 +0100

    Add redirect to edit page when creating new group

--- src/composables/useInitials.ts
+++ src/composables/useInitials.ts
@@ -1,8 +1,10 @@
 export default function useInitials(name: string | null | undefined) {
   if (!name) return ""
   if (name === "New group") return ""
+  // just use the first two letters cut the rest
   return name
     .split(" ")
     .map((word) => word[0])
+    .splice(0, 2)
     .join("")
 }
