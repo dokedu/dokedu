78420237b3439c06766d4c275cac8166c63832fc
Tom Härter <tom@dokedu.org>
2024-01-23 02:23:34 +0100

    Update createReport function in record/reports/new.vue

--- src/pages/record/reports/new.vue
+++ src/pages/record/reports/new.vue
@@ -1,3 +1,113 @@
+<script lang="ts" setup>
+import PageHeader from "@/components/page-header.vue"
+import PageWrapper from "@/components/page-wrapper.vue"
+import PageContent from "@/components/page-content.vue"
+import { Save, Info } from "lucide-vue-next"
+import dButton from "@/components/d-button/d-button.vue"
+import ReportTypeList from "@/components/d-report/d-report-type-list.vue"
+import { computed, reactive, ref } from "vue"
+import dInput from "@/components/d-input/d-input.vue"
+import DCheckbox from "@/components/d-checkbox/d-checkbox.vue"
+import { useRouter } from "vue-router/auto"
+import DSelect from "@/components/d-select/d-select.vue"
+import { createNotification } from "@/composables/useToast"
+import { array, boolean, date, object, string } from "yup"
+import { useCreateReportMutation } from "@/gql/mutations/reports/createReport"
+import { useGetEntryFilterStudentsQuery } from "@/gql/queries/users/getEntryFilterStudents"
+import { useRouteQuery } from "@vueuse/router"
+import DTag from "@/components/d-tag/d-tag.vue"
+import { useCompetenceSubjectsQuery } from "@/gql/queries/competences/competenceSubjects"
+
+const router = useRouter()
+
+const student = useRouteQuery<string>("student")
+const from = useRouteQuery<string>("from")
+const to = useRouteQuery<string>("to")
+const type = useRouteQuery<string>("type", "entries")
+const competence = useRouteQuery<string>("competence")
+const competenceSearch = ref<string>("")
+
+const allUsers = ref(false)
+
+const { executeMutation: createReportMutation } = useCreateReportMutation()
+
+const { data: competenceData } = useCompetenceSubjectsQuery({
+  variables: reactive({
+    search: competenceSearch
+  })
+})
+
+const tagOptions = computed(
+  () =>
+    competenceData?.value?.competences?.edges?.map((edge: any) => ({
+      label: edge.name,
+      value: edge.id
+    })) || []
+)
+
+const studentSearch = ref("")
+const { data: studentData } = useGetEntryFilterStudentsQuery({
+  variables: reactive({
+    search: studentSearch
+  })
+})
+
+const studentOptions = computed(() => [
+  {
+    label: "Alle",
+    value: "-1"
+  },
+  ...(studentData?.value?.users?.edges?.map((edge: any) => ({
+    label: `${edge.firstName} ${edge.lastName}`,
+    value: edge.id
+  })) || [])
+])
+
+const createReportInput = object({
+  studentUser: string(),
+  allUsers: boolean().required(),
+  from: date().required(),
+  to: date().required(),
+  kind: string().required(),
+  format: string().required(),
+  filterTags: array().ensure()
+})
+
+async function createReport() {
+  const scope = student.value === "-1" ? { allUsers: true } : { studentUser: student.value }
+
+  const value = {
+    from: from.value.slice(0, 10),
+    to: to.value.slice(0, 10),
+    kind: type.value,
+    format: "pdf",
+    filterTags: [],
+    ...scope
+  }
+
+  if (!(await createReportInput.isValid(value))) {
+    return createNotification({
+      title: "Formular ungültig",
+      description: "Bitte überprüfe deine Eingaben"
+    })
+  }
+
+  const input = createReportInput.cast(value)
+
+  // @ts-expect-error
+  const { error } = await createReportMutation({ input })
+
+  if (error) {
+    return createNotification({
+      title: "Beim Erstellen des Berichts ist ein Fehler aufgetreten",
+      description: error.message
+    })
+  }
+
+  await router.push({ name: "/record/reports/" })
+}
+</script>
+
 <template>
   <PageWrapper>
     <PageHeader>
@@ -50,10 +160,6 @@
                     <d-tag :color="competenceData?.competences.edges?.find((el: any) => el.id === competence)?.color">
                       {{ competenceData?.competences.edges?.find((el: any) => el.id === competence)?.name }}
                     </d-tag>
-                    <!-- <div class="mb-1 text-sm font-medium">{{ types.find((t) => t.kind === type)?.label }}</div> -->
-                    <!-- <div class="text-xs text-neutral-500"> -->
-                    <!-- {{ types.find((t) => t.kind === type)?.description }} -->
-                    <!-- </div> -->
                   </div>
                   <div v-else class="text-sm text-neutral-600 p-1">Wähle ein Fach aus</div>
                 </template>
@@ -70,107 +176,3 @@
     </PageContent>
   </PageWrapper>
 </template>
-
-<script lang="ts" setup>
-import PageHeader from "@/components/page-header.vue"
-import PageWrapper from "@/components/page-wrapper.vue"
-import PageContent from "@/components/page-content.vue"
-import { DownloadCloud, Save, Info } from "lucide-vue-next"
-import dButton from "@/components/d-button/d-button.vue"
-import ReportTypeList from "@/components/d-report/d-report-type-list.vue"
-import { computed, reactive, ref } from "vue"
-import dInput from "@/components/d-input/d-input.vue"
-import { useRouter } from "vue-router/auto"
-import DSelect from "@/components/d-select/d-select.vue"
-import { createNotification } from "@/composables/useToast"
-import { array, date, object, string } from "yup"
-import { useCreateReportMutation } from "@/gql/mutations/reports/createReport"
-import { useGetEntryFilterStudentsQuery } from "@/gql/queries/users/getEntryFilterStudents"
-import { useRouteQuery } from "@vueuse/router"
-import DTag from "@/components/d-tag/d-tag.vue"
-import { useCompetenceSubjectsQuery } from "@/gql/queries/competences/competenceSubjects"
-
-const router = useRouter()
-
-const student = useRouteQuery<string>("student")
-const from = useRouteQuery<string>("from")
-const to = useRouteQuery<string>("to")
-const type = useRouteQuery<string>("type", "entries")
-const competence = useRouteQuery<string>("competence")
-const competenceSearch = ref<string>("")
-
-const { executeMutation: createReportMutation } = useCreateReportMutation()
-
-const { data: competenceData } = useCompetenceSubjectsQuery({
-  variables: reactive({
-    search: competenceSearch
-  })
-})
-
-const tagOptions = computed(
-  () =>
-    competenceData?.value?.competences?.edges?.map((edge: any) => ({
-      label: edge.name,
-      value: edge.id
-    })) || []
-)
-
-const studentSearch = ref("")
-const { data: studentData } = useGetEntryFilterStudentsQuery({
-  variables: reactive({
-    search: studentSearch
-  })
-})
-
-const studentOptions = computed(() => [
-  // {
-  //   label: "Alle",
-  //   value: "-1"
-  // },
-  ...(studentData?.value?.users?.edges?.map((edge: any) => ({
-    label: `${edge.firstName} ${edge.lastName}`,
-    value: edge.id
-  })) || [])
-])
-
-const createReportInput = object({
-  studentUser: string().required(),
-  from: date().required(),
-  to: date().required(),
-  kind: string().required(),
-  format: string().required(),
-  filterTags: array().ensure()
-})
-
-async function createReport() {
-  const value = {
-    studentUser: student.value,
-    from: from.value.slice(0, 10),
-    to: to.value.slice(0, 10),
-    kind: type.value,
-    format: "pdf",
-    filterTags: []
-  }
-
-  if (!(await createReportInput.isValid(value))) {
-    return createNotification({
-      title: "Formular ungültig",
-      description: "Bitte überprüfe deine Eingaben"
-    })
-  }
-
-  const input = createReportInput.cast(value)
-
-  // @ts-expect-error
-  const { error } = await createReportMutation({ input })
-
-  if (error) {
-    return createNotification({
-      title: "Beim Erstellen des Berichts ist ein Fehler aufgetreten",
-      description: error.message
-    })
-  }
-
-  await router.push({ name: "/record/reports/" })
-}
-</script>
