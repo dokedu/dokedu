92dc5e198735291ae275a289cbf66b9a76ec8404
Tom Härter <tomuhaerter@gmail.com>
2025-04-11 15:10:24 +0200

    Refactor group query by removing unused imports and query schema

--- server/api/groups/index.get.ts
+++ server/api/groups/index.get.ts
@@ -1,18 +1,10 @@
 import { groups } from "../../database/schema"
-import { gt, isNull } from "drizzle-orm"
+import { isNull } from "drizzle-orm"
 import { z } from "zod"
-
-const table = "groups"
-
-const querySchema = z.object({
-  updatedAt: z.coerce.date().optional()
-})
 
 export default defineEventHandler(async (event) => {
   const { user, secure } = await requireUserSession(event)
   if (!secure) throw createError({ statusCode: 401, message: "Unauthorized" })
-
-  const { updatedAt } = await getValidatedQuery(event, querySchema.parse)
 
   const query = useDrizzle()
     .select()
